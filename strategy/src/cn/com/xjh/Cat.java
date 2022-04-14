eda82b32b88996d27ff187e5a969a3c2945ddd03
徐嘉辉 <9636902+wall-mijunshi@user.noreply.gitee.com>
2022-04-14 14:05:31 +0800

    策略模式01： 想要通过一个方法来进行某些策略。如（排序） 在类中定义compareTo策略，然后调用方法排序

---
+++ strategy/src/cn/com/xjh/Cat.java
@@ -0,0 +1,32 @@
+package cn.com.xjh;
+
+/**
+ * @author xujiahui
+ * @create 2022/4/14 13:57
+ */
+public class Cat {
+    int weight,height;
+
+    public Cat(int weight, int height) {
+        this.weight = weight;
+        this.height = height;
+    }
+
+    public int compareTo(Cat cat){
+        if(this.weight>cat.weight){
+            return 1;
+        }else if(this.weight<cat.weight){
+            return -1;
+        }else{
+            return 0;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Cat{" +
+            "weight=" + weight +
+            ", height=" + height +
+            '}';
+    }
+}
---
+++ strategy/src/cn/com/xjh/Main.java
@@ -0,0 +1,12 @@
+package cn.com.xjh;
+
+/**
+ * @author xujiahui
+ * @create 2022/4/14 13:59
+ */
+public class Main {
+    public static void main(String[] args) {
+
+
+    }
+}

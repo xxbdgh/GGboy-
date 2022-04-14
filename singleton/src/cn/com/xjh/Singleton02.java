92d743690de043136849db552bf321432b5794bb
徐嘉辉 <9636902+wall-mijunshi@user.noreply.gitee.com>
2022-04-14 13:48:35 +0800

    单例模式 03: 单例模式指令重排问题解释

--- singleton/src/cn/com/xjh/Singleton02.java
+++ singleton/src/cn/com/xjh/Singleton02.java
@@ -11,7 +11,19 @@ import java.util.concurrent.*;
  */
 public class Singleton02 {
 
-    private static Singleton02 INSTANCE;
+    /**
+     * 使用这种方式使用单例，需要加上volatile关键字，因为有指令重排问题
+     * 指令重排 ： new Singleton时不是一个原子性操作，是由三个操作，合并成一个操作
+     * 1. 分配对象内存空间
+     * 2. 初始化对象
+     * 3. 将对象指向分配的内存空间
+     * 如果这三个操作没有按照顺序执行，则可能出现指令重排的问题
+     * 如果第一个操作没有执行完，第二个操作就开始执行，第二个操作执行完，第三个操作就开始执行，
+     * 这样就可能出现第二个操作执行完，第三个操作执行之前，第一个操作还没有执行完的情况
+     * 比如某一线程 判断时，前一个操作先指向了内存空间，那么判断的内存空间则不为null，
+     * 返回时就会返回一个空值
+     */
+    private static volatile Singleton02 INSTANCE;
 
     public Singleton02() {
     }

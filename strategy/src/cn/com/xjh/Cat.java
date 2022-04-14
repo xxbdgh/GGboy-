b707391cf54b38cde122a1597f5621fa520cb816
徐嘉辉 <9636902+wall-mijunshi@user.noreply.gitee.com>
2022-04-14 14:11:23 +0800

    策略模式02：多种类型同时使用此策略，创建策略接口，使他们都实现接口即可调用策略方法

--- strategy/src/cn/com/xjh/Cat.java
+++ strategy/src/cn/com/xjh/Cat.java
@@ -4,7 +4,7 @@ package cn.com.xjh;
  * @author xujiahui
  * @create 2022/4/14 13:57
  */
-public class Cat {
+public class Cat implements Comparable {
     int weight,height;
 
     public Cat(int weight, int height) {
@@ -12,7 +12,9 @@ public class Cat {
         this.height = height;
     }
 
-    public int compareTo(Cat cat){
+    @Override
+    public int compareTo(Object c){
+        Cat cat = (Cat)c;
         if(this.weight>cat.weight){
             return 1;
         }else if(this.weight<cat.weight){
---
+++ strategy/src/cn/com/xjh/Comparable.java
@@ -0,0 +1,15 @@
+package cn.com.xjh;
+
+/**
+ * @author xujiahui
+ * @create 2022/4/14 14:09
+ */
+public interface Comparable {
+
+    /**
+     * 比较两个对象的大小
+     * @param obj 要比较的对象
+     * @return
+     */
+    int compareTo(Object obj);
+}

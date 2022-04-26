6b6ae624176569fef34eed6b5ef72945c70a0f78
xujiahui <9636902+wall-mijunshi@user.noreply.gitee.com>
2022-04-26 23:15:20 +0800

    多线程: volatile关键字 当属性进行修改之后 强行使缓存失效，重新读取内存中的属性值，刷新属性 使其他线程立即可见

--- java-core/src/cn/com/xjh/Main1.java
+++ java-core/src/cn/com/xjh/Main1.java
@@ -6,10 +6,21 @@ import java.util.Base64;
  * @author xujiahui
  * @create 2022/4/20 21:18
  */
-@Anno1(value="aaa",color="red",show=@Anno2("bbb"))
 public class Main1 {
-   public static void main(String[] args) {
-      //现在的时间
+   public volatile static boolean flag = false;
 
+   public static void main(String[] args) throws InterruptedException {
+      //创建一个线程循环
+      Thread thread = new Thread(()->{
+         int i = 0;
+         while (!flag){
+            i++;
+         }
+         System.out.println("i="+i);
+      });
+      thread.start();
+      System.out.println("Thread start");
+      Thread.sleep(1000);
+      flag = true;
    }
 }
